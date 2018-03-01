de6a04c614d52eef9749cc38eb047d3dea475f53
wanderling <hitchhikerz@outlook.com>
2018-03-01 16:52:17 +0530

    faculty-page

--- js/main.js
+++ js/main.js
@@ -21,3 +21,43 @@
         px=$(window).height()*vh;
         return (px/$(window).width());
     }
+
+    f_1=false;
+    f_2=false;
+    function showMore(val){
+        if(val==1){
+            if(!f_1){
+                $('.fil1-hid').slideDown();
+                $("#sh-mr1").text('Show less');
+                f_1=true;
+            }else{
+                $('.fil1-hid').slideUp();
+                $("#sh-mr1").text('Show more');
+                f_1=false;
+            }
+        }else{
+            if(!f_2){
+                $('.fil2-hid').slideDown();
+                $("#sh-mr2").text('Show less');
+                f_2=true;
+            }else{
+                $('.fil2-hid').slideUp();
+                $("#sh-mr2").text('Show more');
+                f_2=false;
+            }
+        }
+    }
+
+    function fac_viewMore(cls,ths){
+        
+        
+        
+        if($('#'+ths).text()=='more'){
+            $('.'+cls).slideDown();
+            $('#'+ths).text('less');
+        }else{
+            $('.'+cls).slideUp();
+            $('#'+ths).text('more');
+        }
+
+    }
